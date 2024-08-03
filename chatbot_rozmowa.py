7a77c78000ecd6a6bb6766ab46d5a9ecd0ca77c5
Holmings <79854259+Holmings@users.noreply.github.com>
2024-08-03 12:56:03 +0200

    Add files via upload

---
+++ chatbot_rozmowa.py
@@ -0,0 +1,55 @@
+import random
+import json
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+# Wczytaj plik intents.json
+with open('intents.json', encoding='utf-8') as file:
+    intents = json.load(file)
+
+# Przygotowanie listy tagów (klas)
+classes = [intent['tag'] for intent in intents['intents']]
+num_labels = len(classes)
+
+# Załaduj tokenizer i model lokalnie
+tokenizer = AutoTokenizer.from_pretrained("./local_model_2")
+model = AutoModelForSequenceClassification.from_pretrained("./local_model_2", num_labels=num_labels)
+
+tag2idx = {tag: idx for idx, tag in enumerate(classes)}
+idx2tag = {idx: tag for tag, idx in tag2idx.items()}
+
+def tokenize_input(text, tokenizer, max_length=128):
+    return tokenizer.encode_plus(
+        text,
+        add_special_tokens=True,
+        max_length=max_length,
+        padding='max_length',
+        return_attention_mask=True,
+        return_tensors='pt',
+        truncation=True
+    )
+
+def predict_class(text):
+    tokens = tokenize_input(text, tokenizer)
+    input_ids = tokens['input_ids']
+    attention_mask = tokens['attention_mask']
+
+    model.eval()
+    with torch.no_grad():
+        outputs = model(input_ids, attention_mask=attention_mask)
+        logits = outputs.logits
+
+    predicted_class_idx = torch.argmax(logits, dim=1).item()
+    return idx2tag[predicted_class_idx]
+
+def get_response(intents, intent_tag):
+    for intent in intents['intents']:
+        if intent['tag'] == intent_tag:
+            return random.choice(intent['responses'])
+
+# Test działania
+while True:
+    message = input("Ty: ")
+    intent_tag = predict_class(message)
+    response = get_response(intents, intent_tag)
+    print(f"Chatbot: {response}")
