ed987520a4da90d0beec01486b58ff4df0454ede
Holmings <patrycjax@student.agh.edu.pl>
2024-09-09 22:24:30 +0200

    jeszcze literówke poprawiłam

--- static/js/sprawcy.js
+++ static/js/sprawcy.js
@@ -1,4 +1,4 @@
- function areas() {
+function areas() {
     var inputValue = document.getElementById("liczba").value;
     var divek = document.getElementById("divek");
     divek.innerHTML = ""; // Wyczyszczenie zawartości divek przed dodaniem nowych pól tekstowych
@@ -9,9 +9,11 @@
         textarea.setAttribute("placeholder", "W tym miejscu opisz " + i + ". sprawcę...");
         textarea.setAttribute("id", "sprawca" + i);
         textarea.setAttribute("name", "sprawca" + i);
+        textarea.classList.add('sprawca'); // Dodajemy klasę zamiast id
         divek.appendChild(textarea);
         divek.appendChild(br);
     }
+
 }
 
 // Wywołanie funkcji areas() przy zmianie wartości pola liczba
