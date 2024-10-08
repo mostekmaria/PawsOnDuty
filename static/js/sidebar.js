afb7810835dac014f65a23973d9d0fdd1334e8a5
mostekmaria <mostekmaria@student.agh.edu.pl>
2024-10-08 13:44:36 +0200

    poprawiony sidebar

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,10 +1,20 @@
 // Sidebar podstawie https://www.w3schools.com/howto/howto_css_fixed_sidebar.asp
-function openNav() {
-  document.getElementById("mySidebar").style.width = "320px";
-  document.getElementById("main").style.marginRight = "320px";
+function openCloseNav() {
+  const sidebar = document.getElementById("mySidebar");
+    
+  // Jeśli sidebar jest otwarty (ma szerokość 250px), zamknij go
+  if (sidebar.style.width === "290px") {
+      sidebar.style.width = "0";
+  } else {
+      // W przeciwnym razie otwórz sidebar
+      sidebar.style.width = "290px";
+  }
 }
 
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginRight= "0";
-}
+
+window.addEventListener("resize", function() {
+  const sidebar = document.getElementById("mySidebar");
+  if (window.innerWidth > 1100) {
+      sidebar.style.width = "0"; // Sidebar znika, gdy ekran jest większy
+  }
+});
